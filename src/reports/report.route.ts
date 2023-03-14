e43b8264589726f78a56f8b19ab59a6c49896d39
pinsara.ecyber <pinsara@ecyber.com>
2023-03-14 05:53:54 +0530

    create routes for reports

--- src/reports/report.route.ts
+++ src/reports/report.route.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { validate } from '../common/middlewares/validate';
+import { createReport, getAllReports, getReport } from './report.controller';
+import { CreateReportSchema } from './report.schema';
 
 const ReportRouter = express.Router();
 
-ReportRouter.get('/report/:id', );
+ReportRouter.get('/report/', getAllReports);
+ReportRouter.get('/report/:id', getReport);
+ReportRouter.post('/report/', validate(CreateReportSchema), createReport);
 
 export default ReportRouter;
